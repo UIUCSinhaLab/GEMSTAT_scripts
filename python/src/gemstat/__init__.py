8a66d97dd76716f44d824e4ff17607db7d77ecef
Bryan Lunt <bjlunt2@illinois.edu>
2021-10-14 11:40:01 -0500

    imports work

--- python/src/gemstat/__init__.py
+++ python/src/gemstat/__init__.py
@@ -1,1 +1,7 @@
-import matrix, model, motifs, pwm, scores, seqannotation, snot
+from . import matrix
+from . import model
+from . import motifs
+from . import pwm
+from . import scores
+from . import seqannotation
+from . import snot
